7efbb599ea0db15b27e736969a004292615ce891
niudazhuang <dengwm666@icloud.com>
2018-04-29 17:28:10 +0800

    -

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,6 @@
 
 set -o errexit -o nounset
 
-if [ "$TRAVIS_BRANCH" != "master" ]
-then
-  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
-  exit 0
-fi
-
 rev=$(git rev-parse --short HEAD)
 
 cd _site
@@ -15,10 +9,10 @@ echo "now: $PWD"
 
 git init
 
-git config --global user.name "niuDazhuang" 
+git config --global user.name "niuDazhuang"
 git config --global user.email "dengwm666@icloud.com"
 
-git remote add upstream https://$GH_TOKEN@github.com/niuDazhuang/gp.git
+git remote add upstream "https://$GH_TOKEN@github.com/niuDazhuang/gp.git"
 git fetch upstream && git reset upstream/gh-pages
 
 echo `env` > travis.env
